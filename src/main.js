f1dc923bfdeadf6c46529de5da1f14589510284f
strophy <leon.white@dash.org>
2020-02-13 11:34:15 -0800

    Pass props from consuming element to component

--- src/main.js
+++ src/main.js
@@ -5,16 +5,32 @@ import i18n from './i18n'
 import App from './App.vue'
 import './plugins/axios'
 
+var lang = '';
+var citylist = '';
+const appLoader = Vue.component('clean-air-counter', {
+  functional: true,
+  props: [lang, citylist],
+  render: function (createElement, context) {
+    return createElement(App, context.data, context.children);
+  }
+});
 
-Vue.config.productionTip = false
-const mountEl = document.querySelector('#app');
+new Vue({
+  el: '#app',
+  i18n,
+  components: {appLoader}
+});
 
+// Why do I need to nest loaders 3 times like this? Can I move the component to App.vue and put the props there?
+// Why doesn't it render in live build mode anymore? Fix this first
+// Then work out how to store the vars in a sensible place and act on them
+
+
+/*
 new Vue({
   i18n,
   render: createElement => {
-    const context = {
-      props: { ...mountEl.dataset },
-    };
-    return createElement(App, context);
+    return createElement(App);
   }
 }).$mount('#app')
+*/
