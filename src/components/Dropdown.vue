224d1e47ddf2a6dd985eed78e597bb11eb3ba92b
strophy <leon.white@dash.org>
2020-02-13 11:33:43 -0800

    Sort cities according to current language

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -3,27 +3,39 @@
         <option disabled value="select_your_city">
             {{ $t('select_your_city') }}
         </option>
-        <option v-for="option in options" v-bind:key="option" v-bind:value="option">
-            {{ $t('cities.' + option) }}
+        <option v-for="city in sortCities" v-bind:key="city.id" v-bind:value="city.id">
+            {{ city.name }}
         </option>
     </select>
 </template>
 
 <script>
 import cities from './cities.json';
-
+const loadedCities = cities.global;
+var sortedCities = [];
 
 export default {
     name: 'Dropdown',
     methods: {
         optionChanged: function() {
             this.$emit('update:option', this.selected)
-        },
+        }
     },
     data: function() {
         return {
             selected: 'select_your_city',
-            options: cities.global
+            options: cities.global,
+            sortedCities
+        }
+    },
+    computed: {
+        sortCities: function () {
+            sortedCities = [];
+            for (var city in loadedCities) {
+                sortedCities.push({id: loadedCities[city], name: this.$t('cities.' + loadedCities[city])})
+                sortedCities.sort((a, b) => (a.name > b.name) ? 1 : -1);               
+            }
+            return sortedCities
         }
     }
 }
