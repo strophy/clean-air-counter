054b9d063b1d66ab18bd0004c7d2fdb01660a2f9
strophy <leon.white@dash.org>
2020-02-13 18:52:19 -0800

    Add social media share functions

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -1,48 +1,50 @@
 <template>
     <div v-if="this.$parent.cityData">
-        <i18n path="deaths" tag="p" id="costtext">
+        <i18n path="deaths" tag="h2" id="costtext">
             <template #city>
                 <strong class="bg-warning">{{ $t('cities.' + cityData.cityID) }}</strong>
             </template>
             <template #costLine><br />
                 <span class="bg-warning font-weight-bold">{{ Math.round(cityData.estimations.ytd["PM2.5"][1].number_central).toLocaleString() }} {{ $t('life_cost')}}</span>
                 <span class="bg-transparent"> {{ $t('and') }} </span>
-                <span class="bg-warning font-weight-bold">${{ Math.round(cityData.estimations.ytd["PM2.5"][0]["cost.USD_central"]).toLocaleString() }}</span><br />
+                <span class="bg-warning font-weight-bold">US${{ Math.round(cityData.estimations.ytd["PM2.5"][0]["cost.USD_central"]).toLocaleString() }}</span><br />
             </template>
             <template #showDate>
                 <span>{{ $d(new Date(2020, 0, 1), 'long') }}</span>
             </template>
         </i18n>
         <hr />
-        <strong>{{ $t('share') }}</strong><br />
-        <a 
-        class="btn btn-warning m-1"
-        :href=ohai
-        role="button">
-          {{ $t('twitter') }}
-        </a>
-        <a class="btn btn-warning m-1" :href="$t('f_share_url')" role="button">{{ $t('facebook') }}</a>
+        <div>
+            <strong>{{ $t('share') }}</strong><br />
+            <a class="btn btn-warning m-1" href="" role="button">{{ $t('twitter') }}</a>
+            <a class="btn btn-warning m-1" href="" role="button">{{ $t('facebook') }}</a>
+        </div>
     </div>
 </template>
 
 <script>
-const working = "Try using custom formatting https://kazupon.github.io/vue-i18n/guide/formatting.html#custom-formatting"
-const ohai = "https://twitter.com/intent/tweet?text=hello+world&url=https://www.airvisual.com/&hashtags=CleanAirNow"
-
+/* eslint-disable */
+var twitterShare = '';
+var facebookShare = '';
 export default {
     name: 'ShowData',
     props: ['cityData'],
     data: function() {
         return {
-            ohai,
-            working
         }
+    },
+    updated: function () {
+        var twitterBase = "https://twitter.com/intent/tweet?";
+        var facebookBase = "https://www.facebook.com/sharer/sharer.php?";
+
+        var shareText = document.getElementById("costtext").textContent;
+        var shareUrl = "https://www.airvisual.com/";
+        var shareHashtag = this.$t('hashtag');
+
+        twitterShare = twitterBase + 'text=' + shareText + '&url=' + shareUrl + '&hashtags=' + shareHashtag;
+        facebookShare = facebookBase + 'u=' + shareUrl;
+        console.log(encodeURI(twitterShare))
+        console.log(encodeURI(facebookShare))
     }
 }
 </script>
-
-<style>
-    #costtext{
-        font-size: 1.5rem
-    }
-</style>
