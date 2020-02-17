e8f14ac2136859859aa08b546ea1f5ec4c84aa7e
strophy <leon.white@dash.org>
2020-02-17 14:24:44 -0800

    Move calculations to methods

--- src/components/ShowData.vue
+++ src/components/ShowData.vue
@@ -5,9 +5,9 @@
                 <strong class="bg-warning">{{ $t('cities.' + cityData.cityID) }}</strong>
             </template>
             <template #costLine><br />
-                <span class="bg-warning font-weight-bold">{{ Math.round(cityData.estimations.ytd["PM2.5"][1].number_central).toLocaleString() }} {{ $t('life_cost')}}</span>
+                <span class="bg-warning font-weight-bold">{{ totalDeaths(cityData).toLocaleString() }} {{ $t('life_cost')}}</span>
                 <span class="bg-transparent"> {{ $t('and') }} </span>
-                <span class="bg-warning font-weight-bold">US${{ Math.round(cityData.estimations.ytd["PM2.5"][0]["cost.USD_central"]).toLocaleString() }}</span><br />
+                <span class="bg-warning font-weight-bold">US${{ totalCosts(cityData).toLocaleString() }}</span><br />
             </template>
             <template #showDate>
                 <span>{{ $d(new Date(2020, 0, 1), 'long') }}</span>
@@ -30,8 +30,29 @@ export default {
         return {
         }
     },
+    methods: {
+        totalDeaths: function(cityData) {
+            // Get all data resulting in deaths
+            var pm25Deaths = cityData.estimations.ytd['PM2.5'].filter(x => x.Outcome === 'Deaths')
+            var no2Deaths = cityData.estimations.ytd['NO2'].filter(x => x.Outcome === 'Deaths')
+            var o3Deaths = cityData.estimations.ytd['O3_8h'].filter(x => x.Outcome === 'Deaths')
+            var totalDeaths = 0
+
+            // Add up deaths and check for existence of other causes of death
+            for (var death in pm25Deaths) {
+                totalDeaths += pm25Deaths[death].number_central
+            }
+            totalDeaths += (no2Deaths[0].number_central ? no2Deaths[0].number_central : 0) + (o3Deaths[0].number_central ? o3Deaths[0].number_central : 0)
+            
+            return Math.round(totalDeaths)
+        },
+        totalCosts: function(cityData) {
+            var pm25Costs = cityData.estimations.ytd['PM2.5'].find(x => x.Outcome === 'Absences')
+            return Math.round(pm25Costs['cost.USD_central'])
+        }
+    },
     updated: function () {
-        var shareText = document.getElementById("costtext").textContent;
+        var shareText = document.getElementById('costtext').textContent;
         var shareHashtag = this.$t('hashtag');
         var twitterShare = 'https://twitter.com/intent/tweet?text=' + shareText + '&url=' + window.location.href + '&hashtags=' + shareHashtag;
         var facebookShare = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href;
